278d298e54db1c3d1730b56397d47ac8b121cf55
Javier Arce <javierarce@gmail.com>
2012-03-01 19:45:58 +0100

    Removed old progress-bars files. Adds small switch

--- js/app.js
+++ js/app.js
@@ -5,20 +5,39 @@ $(document).ready(function() {
     });
 
     $(".translucent-box").on("mouseenter", function() {
-        $(this).animate({top:0}, { duration: 200, easing: "easeInOutExpo" });
+        $(this).stop().animate({top:0}, { duration: 200, easing: "easeInOutExpo" });
     });
 
     $(".translucent-box").on("mouseleave", function() {
-        $(this).animate({top:"160px"}, { duration: 200, easing: "easeInOutExpo" });
+        $(this).stop().animate({top:"160px"}, { duration: 200, easing: "easeInOutExpo" });
     });
 
     $(".switch a").on("click", function(e) {
         e.preventDefault();
-    
-    var $a = $(this);
-    $(".bkg").animate({right: 120 - $(this).position().left }, 100, function() {
-        $(".switch").find("a").removeClass("selected");
-        $a.addClass("selected");   
-    }); 
-});
+
+        var $a = $(this);
+        $(".bkg").animate({right: 120 - $(this).position().left }, 100, function() {
+            $(".switch").find("a").removeClass("selected");
+            $a.addClass("selected");   
+            if ($(this).position().left == 0) {
+                $(".collection-list li.mine").animate({opacity:.2}, { duration: 100});
+            } else {
+                $(".collection-list li.mine").animate({opacity:1}, { duration: 100});
+            }
+        }); 
+    });
+
+    $("span.switch").on("click", function(e) {
+        e.preventDefault();
+
+        var $a = $(this);
+            if ($(this).hasClass("selected")) {
+                $(this).toggleClass("selected");
+                $(".collection-list li.completed").animate({opacity:.2}, { duration: 100});
+            } else {
+                $(this).toggleClass("selected");
+                $(".collection-list li.completed").animate({opacity:1}, { duration: 100});
+            }
+    });
+
 });
